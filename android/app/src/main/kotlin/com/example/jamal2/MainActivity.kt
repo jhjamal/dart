310fbe45619fef54877cae40ce7c5dcfa673c8dc
jhjamal <jamalhossain9358@gmail.com>
2022-12-21 10:29:45 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/jamal2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jamal2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/testclass2/jamal2.dart
@@ -0,0 +1,50 @@
+
+
+void main() {
+
+  myname();
+
+  jogbiog();
+  functionName();
+
+}
+
+myname(){
+  print("Hello brother");
+  var name = "Jamal";
+  print(name);
+
+  var firstName = "Jamal";
+  var lastName = "Hossain";
+  print("My name is $firstName $lastName");
+}
+
+jogbiog(){
+  int num1 = 10;
+  int num2 = 3;
+
+  //calculation
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div = num1/ num2;
+  print("The sum is $sum");
+  print("The diff is $diff");
+  print("The mul is $mul");
+  print("The div is $div");
+}
+
+
+functionName(){
+  String name = "Kamal";
+  String address = "BD";
+  num age = 22;
+  num height = 5.5;
+  bool Married = false;
+  print ("Name is $name");
+  print ("Address is $address");
+  print ("Age is $age");
+  print ("Height is $height");
+  print ("Marriad status is $Married");
+}
+
